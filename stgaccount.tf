2e44c1fee83f1972a9e3e007b9e4fb9f2443a8c7
Wasimbkhan <wasimbkhan1991@outlook.com>
2025-03-02 14:36:13 +0530

    created stg account to store csv file

---
+++ stgaccount.tf
@@ -0,0 +1,23 @@
+
+resource "azurerm_storage_account" "wasim_stg" {
+  name                     = "wasim_stg"
+  resource_group_name      = azurerm_resource_group.rgone.name
+  location                 = azurerm_resource_group.rgone.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+
+}
+
+resource "azurerm_storage_container" "wasim_cont" {
+  name                  = "vhds"
+  storage_account_id    = azurerm_storage_account.wasim_stg.name
+  container_access_type = "private"
+}
+
+resource "azurerm_storage_blob" "vm_output_blob" {
+  name                   = "vm_output.csv"
+  storage_account_name   = azurerm_storage_account.wasim_stg.name
+  storage_container_name = azurerm_storage_container.wasim_cont.name
+  type                   = "Block"
+  source                 = "/home/vsts/work/1/vm_output.csv"
+}
