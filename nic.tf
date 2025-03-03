a1c2f623f3c52d2b7fb2e4a08b639fbd86ff6166
Wasimbkhan <wasimbkhan1991@outlook.com>
2025-03-03 14:47:05 +0530

    corrected for loop local veriable

--- nic.tf
+++ nic.tf
@@ -25,7 +25,7 @@ resource "azurerm_network_interface" "nic" {
     name                          = "internal"
     subnet_id                     = azurerm_subnet.subnet.id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id = azurerm_public_ip.myvm_pip.id
+    public_ip_address_id = [azurerm_public_ip.myvm_pip[each.key].id]
   }
 }
 
